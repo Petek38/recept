e8dec944baa3191e38383e601136ba1b5cf90106
Petek38 <nejc.petek102@gmail.com>
2026-01-27 07:51:56 +0000

    a

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    ingredient_list = []
+    for i in range(1,21):
+        call=requests.get("https://www.themealdb.com/api/json/v1/1/random.php").json()
+        if call["meals"][0][f"strIngredient{i}"]:
+            ingredient = call["meals"][0].get(f"strIngredient{i}")
+            measurement = call["meals"][0].get(f"strMeasure{i}")
+            if ingredient and measurement:  # Only add if both ingredient and measurement are present
+                ingredient_list.append((ingredient, measurement))
+    
+    return render_template("index.html", title="Hello", slika=call["meals"][0]["strMealThumb"], backColor=pridobi_barvo(call["meals"][0]["strMealThumb"],1))
+
+
+def pridobi_barvo(url_slike: str, st_barv: int = 2) -> list:
+    """Funkcija za pridobivanje dominantnih barv iz slike.
+    
+    Primer uporabe:
+    barve = pridobi_barvo("https://example.com/slika.jpg")
+    print(barve[0])  # prva (najbolj dominantna) barva
+    """
+    from dominantcolors import get_image_dominant_colors
+    import urllib.request
+    
+    temp_slika = "static/temp_slika.jpg"
+    try:
+        urllib.request.urlretrieve(url_slike, temp_slika)
+        return get_image_dominant_colors(image_path=temp_slika, num_colors=st_barv)
+    except Exception as e:
+        print(f"Napaka: {e}")
+        return [(255, 255, 255)]  # Vrnemo belo barvo v primeru napake
+
+
+
+app.run(debug=True, port=5002)
